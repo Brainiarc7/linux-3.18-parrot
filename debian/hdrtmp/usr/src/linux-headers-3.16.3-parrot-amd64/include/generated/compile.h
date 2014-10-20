439ddafd975b837d05c86a527e5ac70ec203f27a
EclipseSpark <eclipse-github@frozenbox.org>
2014-10-20 22:43:36 +0200

    first upload from local kernel tree

---
+++ debian/hdrtmp/usr/src/linux-headers-3.16.3-parrot-amd64/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#2 SMP Mon Oct 20 22:09:07 CEST 2014"
+#define LINUX_COMPILE_BY "eclipse"
+#define LINUX_COMPILE_HOST "parrot"
+#define LINUX_COMPILER "gcc version 4.7.2 (Debian 4.7.2-5) "
